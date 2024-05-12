fc2266d6cd27bd25c4310ee6a55a5baac66fcb3c
manweljs <manweljs@gmail.com>
2024-05-12 08:28:01 +0800

    update next config

--- api/utils/tokens.py
+++ api/utils/tokens.py
@@ -35,7 +35,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         user_id: str = payload.get("sub")
 
-        print("user_id", user_id)
+        # print("user_id", user_id)
         user = await User.get(id=user_id)
         if user is None:
             raise HTTPException(status_code=401, detail="Invalid token")
